503988bbbed44fb3f12e8b6a624f79d36588cbf4
Roman Kiss <romiki44@gmail.com>
2019-12-30 17:34:35 +0100

    Add project files.

---
+++ ContactWeb2019/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ContactWeb2019.MvcApplication" Language="C#" %>
---
+++ ContactWeb2019/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContactWeb2019.Startup))]
+namespace ContactWeb2019
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
